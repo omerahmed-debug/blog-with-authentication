c7ff91b0d459f335acd1bb479cfb95eb0b8a98c8
4J9060897 <omer.a.ahmed@ibm.com>
2022-06-15 12:08:47 -0500

    My first look of my blog app.Add the .gitgnore

--- templates/main.py
+++ templates/main.py
@@ -31,7 +31,7 @@ def admin_only(f):
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
